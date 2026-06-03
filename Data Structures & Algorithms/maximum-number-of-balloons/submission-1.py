4001d9a0cc81c47101690c419dba0386cd9abb0c
surya-prasanna <surya.prasanna008@gmail.com>
2026-06-03 10:23:47 -0700

    Add: maximum-number-of-balloons - submission-1

---
+++ Data Structures & Algorithms/maximum-number-of-balloons/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        mp = defaultdict(int)
+
+        for chars in text:
+            if chars in "balon":
+                mp[chars] += 1
+        
+        if len(mp) < 5: 
+            return 0 
+
+        mp['l'] //= 2
+        mp['o'] //= 2
+
+        return min(mp.values())
+            
+
+        
