f73394da40dbc5d9e7a06159fb03aaf843bc8a84
surya-prasanna <surya.prasanna008@gmail.com>
2026-06-22 18:09:27 -0700

    Add: longest-palindrome - submission-5

---
+++ Data Structures & Algorithms/longest-palindrome/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        count = Counter(s)
+
+        ans = 0
+        isOne = False 
+
+
+        for item in count: 
+            if count[item] % 2 == 0: 
+                ans += count[item]
+            else: 
+                ans += count[item] - 1
+                isOne = True
+        
+        if isOne:
+            return ans + 1
+        else:
+            return ans
